8791a45ceaad4fb1cc3750b8b29b9b1d5e2b99dc
Marius Lundgård <studio@mariuslundgard.com>
2022-12-07 11:41:32 +0100

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# sanity-ui-workshop-starter
+
+This is an example repo of using `@sanity/ui-workshop` to prototype, develop and test UI components.
+
+```sh
+# use `degit` to start a new project
+npx degit git@github.com:sanity-io/sanity-ui-workshop-starter
+```
---
+++ src/__workshop__/props.tsx
@@ -0,0 +1,19 @@
+import {Box, Card, Container, Text} from '@sanity/ui'
+import {useString} from '@sanity/ui-workshop'
+import {CustomField} from '../CustomField'
+
+export default () => {
+  const title = useString('Title', 'My custom field')
+
+  return (
+    <Container width={1}>
+      <Box paddingX={4} paddingY={[5, 6, 7]}>
+        <CustomField title={title}>
+          <Card border padding={3}>
+            <Text>This is just an example</Text>
+          </Card>
+        </CustomField>
+      </Box>
+    </Container>
+  )
+}
